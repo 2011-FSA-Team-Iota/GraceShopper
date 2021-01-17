d2750aab37e0219824757972ca90b529ec358748
Vincent <vpang@law.fordham.edu>
2021-01-17 11:03:15 -0500

    T1: Test for Delete Route Added

--- test/tier1/tier1.spec.js
+++ test/tier1/tier1.spec.js
@@ -10,8 +10,8 @@ const {
 const _app = require('../../server/index')
 const app = require('supertest')(_app)
 
-describe('Tier 1: GET Routes', () => {
-  describe('Express', () => {
+describe('Tier 1: ', () => {
+  describe('Express Routes', () => {
     beforeEach(async () => {
       await db.sync({force: true})
       await Promise.all([
@@ -28,11 +28,25 @@ describe('Tier 1: GET Routes', () => {
           description: 'Blah Blah',
           imgUrl:
             'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-iphoneX-silver_AV2?wid=1144&hei=1144&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1548459943718'
+        }),
+        Product.create({
+          name: 'iphone jasmine',
+          price: 30999,
+          description: 'Blah Blah',
+          imgUrl:
+            'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-iphoneX-silver_AV2?wid=1144&hei=1144&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1548459943718'
+        }),
+        Product.create({
+          name: 'iphone 100',
+          price: 30999,
+          description: 'Blah Blah',
+          imgUrl:
+            'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-iphoneX-silver_AV2?wid=1144&hei=1144&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1548459943718'
         })
       ])
     })
 
-    describe('GET /api/products', () => {
+    describe('GET - /api/products', () => {
       it('responds with all products', async () => {
         const response = await app.get('/api/products')
         expect(response.status).to.equal(200)
@@ -41,29 +55,20 @@ describe('Tier 1: GET Routes', () => {
         expect(productArr).to.include('iphone mocha')
         expect(productArr).to.include('iphone chai')
       })
+    })
 
-      // it('does not include any teachers in the response', async () => {
-      //   const response = await app.get('/api/users/unassigned');
-      //   const names = response.body.map((user) => user.name);
-      //   expect(names).to.not.include(
-      //     'LUCY',
-      //     'LUCY is a teacher, but was included in the response'
-      //   );
-      //   expect(names).to.not.include(
-      //     'HANNAH',
-      //     'HANNAH is a teacher, but was included in the response'
-      //   );
-      // });
-
-      // it('does not include any students who have a mentor', async () => {
-      //   const response = await app.get('/api/users/unassigned');
-      //   const names = response.body.map((user) => user.name);
-      //   expect(names).to.not.include(
-      //     'MOE',
-      //     'Students with a mentor should not be included'
-      //   );
-      //   expect(response.body.length).to.equal(2);
-      // });
+    describe('DELETE - /api/products/:id', () => {
+      it('deletes an existing product by their id and responds with 204', async () => {
+        let product = {id: 4, name: 'iphone 100'}
+        const response = await app.delete(`/api/products/${product.id}`)
+        expect(response.status).to.equal(204)
+        let iphone100 = await Product.findByPk(product.id)
+        expect(
+          iphone100,
+          'iPhone 100 should have been deleted, but was not'
+        ).to.equal(null)
+        expect(await Product.findAll()).to.have.lengthOf(3)
+      })
     })
   })
 })
