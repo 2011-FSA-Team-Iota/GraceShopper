e747d8e37e4da6f91958c46e22ad87f9cbfa8afa
Edwin Rodriguez <RodriguezEdwin1324@gmail.com>
2021-01-19 01:47:50 -0500

    T1-BE: added DB creation configuration

--- server/db/db.js
+++ server/db/db.js
@@ -6,9 +6,17 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
-    logging: false
+    logging: false,
+    operatorsAliases: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: true
+    }
   }
 )
+
 module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
