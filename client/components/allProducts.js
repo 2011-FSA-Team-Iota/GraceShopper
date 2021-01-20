34e14c90393e99cfed5c1dffa6939aa51e8ea529
Vincent <vpang@law.fordham.edu>
2021-01-20 12:35:22 -0500

    flex-box added

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -12,25 +12,25 @@ export class AllProducts extends React.Component {
     const {isAdmin} = this.props.user
 
     return (
-      <div>
+      <div id="our-products">
         {isAdmin && <Link to="/addproduct">Add Product</Link>}
         <h2>Our Products</h2>
-        {this.props.products.sort((a, b) => b.id - a.id).map(eachProduct => {
-          return (
-            <span key={eachProduct.id}>
-              <Link to={`/products/${eachProduct.id}`}>
-                <h3>{eachProduct.name}</h3>
-              </Link>
-              <span>{eachProduct.price / 100}</span>
-              <Link to={`/products/${eachProduct.id}`}>
-                <img src={eachProduct.imgUrl} style={{height: '100px'}} />
-              </Link>
-              <label>Description: </label>
-              <p>{eachProduct.description}</p>
-              <p>{`${eachProduct.inventory} left in stock.`}</p>
-            </span>
-          )
-        })}
+        <div id="products-wrapper">
+          {this.props.products.sort((a, b) => b.id - a.id).map(eachProduct => {
+            return (
+              <span key={eachProduct.id} className="each-item">
+                <Link to={`/products/${eachProduct.id}`}>
+                  <h3>{eachProduct.name}</h3>
+                </Link>
+                <span>{`$${eachProduct.price / 100}`}</span>
+                <Link to={`/products/${eachProduct.id}`}>
+                  <img src={eachProduct.imgUrl} style={{height: '100px'}} />
+                </Link>
+                <p>{`${eachProduct.inventory} left in stock.`}</p>
+              </span>
+            )
+          })}
+        </div>
       </div>
     )
   }
