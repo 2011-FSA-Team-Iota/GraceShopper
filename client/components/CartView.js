ecfbb269050f1cfb6839769695a2dda6c4f19bda
Vincent <vpang@law.fordham.edu>
2021-01-20 15:50:05 -0500

    Cart View Update - Partial Styling

--- client/components/CartView.js
+++ client/components/CartView.js
@@ -19,49 +19,53 @@ class CartView extends React.Component {
 
   render() {
     let totalPrice = 0
+    this.props.cart.sort((a, b) => b.id - a.id).map(eachProduct => {
+      totalPrice += eachProduct.price * eachProduct.orderProducts.quantity / 100
+    })
 
     return (
-      <div>
-        <h2>Cart</h2>
-        {this.props.cart.length ? (
-          <>
-            {this.props.cart.sort((a, b) => b.id - a.id).map(eachProduct => {
-              totalPrice +=
-                eachProduct.price * eachProduct.orderProducts.quantity / 100
-              return (
-                <div key={eachProduct.id}>
-                  <span>
-                    <Link to={`/products/${eachProduct.id}`}>
-                      <h3>{eachProduct.name}</h3>
-                    </Link>
-                    <span>${eachProduct.price / 100}</span>
-                    <Link to={`/products/${eachProduct.id}`}>
-                      <img src={eachProduct.imgUrl} style={{height: '50px'}} />
-                    </Link>
-                  </span>
-                  <div>
+      <div id="cart-view-wrapper">
+        <div id="cart-view-total">
+          <h2>
+            Cart Total: <span>${totalPrice.toFixed(2)}</span>
+          </h2>
+        </div>
+        <div id="cart-item-list">
+          {this.props.cart.length ? (
+            <>
+              {this.props.cart.sort((a, b) => b.id - a.id).map(eachProduct => {
+                return (
+                  <div key={eachProduct.id} className="each-cart-item">
+                    <span>
+                      <Link to={`/products/${eachProduct.id}`}>
+                        <h3>{eachProduct.name}</h3>
+                      </Link>
+                      <Link to={`/products/${eachProduct.id}`}>
+                        <img src={eachProduct.imgUrl} className="item-image" />
+                      </Link>
+                    </span>
+                    <div>
+                      <span>${eachProduct.price / 100}</span>
+                      <QuantityInput
+                        product={eachProduct}
+                        inventory={eachProduct.inventory}
+                        quantity={eachProduct.orderProducts.quantity}
+                      />
+                    </div>
                     <DeleteCart product={eachProduct} />
-                    <QuantityInput
-                      product={eachProduct}
-                      inventory={eachProduct.inventory}
-                      quantity={eachProduct.orderProducts.quantity}
-                    />
                   </div>
-                </div>
-              )
-            })}
-            <p>
-              Total: <span>${totalPrice.toFixed(2)}</span>
-            </p>
-            <button type="submit" onClick={evt => this.handleSubmit(evt)}>
-              Checkout
-            </button>
-          </>
-        ) : !this.props.cart.length ? (
-          <h1>Cart is empty.</h1>
-        ) : (
-          <h1>LOADING</h1>
-        )}
+                )
+              })}
+              <button type="submit" onClick={evt => this.handleSubmit(evt)}>
+                Checkout
+              </button>
+            </>
+          ) : !this.props.cart.length ? (
+            <h1>Cart is empty.</h1>
+          ) : (
+            <h1>LOADING</h1>
+          )}
+        </div>
       </div>
     )
   }
