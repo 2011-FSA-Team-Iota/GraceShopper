2b7e65b9967af3b502850319f9df623cd3d91840
Steve Song <steve.b.song92@gmail.com>
2021-01-20 19:21:01 -0500

    T1-CSS: LandingView

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import {Jumbotron, Container, Button} from 'reactstrap'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faAppleAlt} from '@fortawesome/free-solid-svg-icons'
 
 const Landing = () => {
   return (
     <div>
       <Jumbotron fluid className="jumbo1">
         <Container fluid>
-          <h1 className="display-3">Hello, world!</h1>
-          <p className="lead">
-            This is a simple hero unit, a simple Jumbotron-style component for
-            calling extra attention to featured content or information.
-          </p>
-          <hr className="my-2" />
-          <p>
-            It uses utility classes for typography and spacing to space content
-            out within the larger container.
-          </p>
-          <p className="lead">
-            <Button color="primary">Learn More</Button>
-          </p>
+          <div
+            style={{
+              marginTop: 5
+            }}
+          >
+            <FontAwesomeIcon icon={faAppleAlt} color="f5d1" size="1x" />
+          </div>
+          <div className="overlay">
+            <h1 style={{marginBottom: 20}}>Welcome to CESV</h1>
+            <p style={{marginLeft: 50, marginRight: 50}}>
+              CESV is an American multinational technology company headquartered
+              in NYC It is considered one of the Big companies in the U.S.
+              information technology industry. Its hardware product includes the
+              iPhone smartphone.
+            </p>
+            {/* <p><Button color="primary">Learn More</Button></p> */}
+          </div>
         </Container>
       </Jumbotron>
     </div>
