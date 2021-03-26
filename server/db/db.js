ea411fa9e6c47962d48880b16b880cecd04ee3a4
Edwin Rodriguez <RodriguezEdwin1324@gmail.com>
2021-03-26 18:34:44 -0400

    sequelize config tweaked for re-deployment to heroku

--- server/db/db.js
+++ server/db/db.js
@@ -3,14 +3,30 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+//   {
+//     logging: false,
+//     // operatorsAliases: false,
+//     dialect: 'postgres',
+//     protocol: 'postgres',
+//     ssl: true
+//   }
+// )
+
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false,
-    operatorsAliases: false,
-    dialect: 'postgres',
-    protocol: 'postgres',
-    ssl: true
+    // operatorsAliases: false,
+    dialect: 'postgres'
+    // ssl: true,
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false
+    //   }
+    // },
   }
 )
 
